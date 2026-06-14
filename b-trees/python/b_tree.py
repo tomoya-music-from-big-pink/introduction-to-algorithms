05890d5bd7cef192187a565ea9e42dfb47b080dc
Tomoya Sasaki <tomoya.metropolis.part2@gmail.com>
2026-06-14 12:48:57 +0900

    [WIP] Implement b tree with Python

--- b-trees/python/b_tree.py
+++ b-trees/python/b_tree.py
@@ -1,5 +1,4 @@
 class Element:
-
     def __init__(self):
         self.keys = []
         self.n = 0
@@ -8,7 +7,6 @@ class Element:
 
 
 class Tree:
-
     def __init__(self, min_degree=2):
         self.min_degree = min_degree
         self.root = Element()
@@ -25,6 +23,58 @@ class Tree:
             self.__insert_key_non_full(s, key)
         else:
             self.__insert_key_non_full(r, key)
+
+    def contains(self, key):
+
+        def contains_internal(x, key):
+            i = 0
+            while i < x.n and x.keys[i] < key:
+                i += 1
+
+            if i < x.n and x.keys[i] == key:
+                return True
+            else:
+                if x.is_leaf:
+                    return False
+                else:
+                    return contains_internal(x.children[i], key)
+
+        return contains_internal(self.root, key)
+
+    def remove(self, key):
+        x = self.contains(key)
+        if not x:
+            print(f"{key} is not found in the tree.")
+
+        self.__remove_internal(self.root, key)
+
+    def __remove_internal(self, x, key):
+        i = 0
+        while i < x.n and x.keys[i] < key:
+            i += 1
+
+        if i < x.n:
+            if x.keys[i] == key:
+                if x.is_leaf:
+                    x.keys.pop(i)
+                    x.n -= 1
+                else:
+                    if x.children[i].n >= self.min_degree:
+                        y = x.children[i]
+                        key_to_lift = y.keys[-1]
+
+                        y.keys = y.keys[:-1]
+                        y.n -= 1
+                        x.keys[i] = key_to_lift
+                    elif x.children[i + 1].n >= self.min_degree:
+                        y = x.children[i + 1]
+                        key_to_lift = y.keys[0]
+
+                        y.keys = y.keys[1:]
+                        y.n -= 1
+                        x.keys[i] = key_to_lift
+        else:
+            self.__remove_internal(x.children[i], key)
 
     def __insert_key_non_full(self, x, key):
         if x.is_leaf:
@@ -57,15 +107,15 @@ class Tree:
 
         key_to_lift = y.keys[self.min_degree - 1]
 
-        z.keys = y.keys[self.min_degree:]
+        z.keys = y.keys[self.min_degree :]
         z.n = len(z.keys)
-        y.keys = y.keys[:self.min_degree - 1]
+        y.keys = y.keys[: self.min_degree - 1]
         y.n = len(y.keys)
 
         z.is_leaf = y.is_leaf
         if not y.is_leaf:
-            z.children = y.children[self.min_degree:]
-            y.children = y.children[:self.min_degree]
+            z.children = y.children[self.min_degree :]
+            y.children = y.children[: self.min_degree]
 
         x.keys.insert(i, key_to_lift)
         x.n = len(x.keys)
@@ -80,13 +130,24 @@ class Tree:
             print("  " * space_size, r.keys)
             if r.children:
                 for i in range(len(r.children)):
-                    print_tree_internal(r.children[i], space_size=space_size+1)
+                    print_tree_internal(r.children[i], space_size=space_size + 1)
 
         print_tree_internal(self.root)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     t = Tree()
+
+    t.insert(5)
+    t.insert(9)
+    t.insert(3)
+    t.insert(7)
+    # t.insert(1)
+    # t.insert(2)
+    # t.insert(8)
+    # t.insert(6)
+    # t.insert(0)
+    # t.insert(4)
 
     while True:
         print("1:insert 2:remove 3:print > ", end="")
@@ -96,7 +157,10 @@ if __name__ == '__main__':
 
             t.insert(key)
         elif op == 2:
-            pass
+            print("input key > ", end="")
+            key = int(input())
+
+            t.remove(key)
         elif op == 3:
             t.print_tree()
         else:
